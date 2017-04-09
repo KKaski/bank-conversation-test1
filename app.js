768e5c56da030209ed5992a2a11b7e176529cc8f
KKaski <kimmo.kaskikallio@fi.ibm.com>
2017-04-09 20:22:18 +0300

    proxy added

--- app.js
+++ app.js
@@ -18,7 +18,7 @@
 
 var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
-var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+const request = require('request-promise');
 
 var app = express();
 
@@ -26,45 +26,26 @@ var app = express();
 app.use(express.static('./public')); // load UI from public folder
 app.use(bodyParser.json());
 
-// Create the service wrapper
-var conversation = new Conversation({
-  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
-  // After  that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  // username: '<username>',
-  // password: '<password>',
-  //url: 'https://gateway.watsonplatform.net/conversation/api',
-  url:'https://bank-conversation-nr.mybluemix.net/conversation-proxy',
-  version_date: '2016-10-21',
-  version: 'v1'
-});
 
 // Endpoint to be call from the client side
 app.post('/api/message', function(req, res) {
-  //var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
 
-/* 
-  if (!workspace || workspace === '<workspace-id>') {
-    return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-      }
-    });
-  }
-  */
-  
-  var payload = {
-    workspace_id: workspace,
-    context: req.body.context || {},
-    input: req.body.input || {}
-  };
+//This is for the proxy call instead of SDK
+const options = {  
+  method: 'POST',
+  uri: 'https://bank-conversation-nr.mybluemix.net/conversation-proxy',
+  json: req.body.input || {}
+}
+ //Call the proxy in node-red
+request(options)  
+  .then(function (response) {
+    console.log(response);
+    return res.json(updateMessage(response));
+  })
+  .catch(function (err) {
+    return res.status(err.code || 500).json(err);
+  })
 
-  // Send the input to the conversation service
-  conversation.message(payload, function(err, data) {
-    if (err) {
-      return res.status(err.code || 500).json(err);
-    }
-    return res.json(updateMessage(payload, data));
-  });
 });
 
 /**
@@ -73,7 +54,7 @@ app.post('/api/message', function(req, res) {
  * @param  {Object} response The response from the Conversation service
  * @return {Object}          The response with the updated message
  */
-function updateMessage(input, response) {
+function updateMessage(response) {
   var responseText = null;
   if (!response.output) {
     response.output = {};
